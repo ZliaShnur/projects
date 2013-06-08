8d396f9b923e866ac8400f73620190e4c5132d8b
Boris <bonbonev@gmail.com>
2013-06-08 10:15:07 +0300

    DSA - Dictonaries-Hash-Tables-and-Sets
    Task 6

---
+++ DSA - Dictonaries-Hash-Tables-and-Sets/PhoneBook.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wintellect.PowerCollections;
+
+class PhoneBook
+{
+    private MultiDictionary<string, string> phoneBook = new MultiDictionary<string, string>(true);
+
+    public PhoneBook (string inputFile)
+	{
+        string[] input = System.IO.File.ReadAllLines(@"..\..\phones.txt");        
+
+        foreach (var line in input)
+        {
+            string name = line.Split('|')[0].TrimEnd(' ');
+            string town = line.Split('|')[1].TrimEnd(' ');
+            string phone = line.Split('|')[2].TrimEnd(' ');
+            this.phoneBook.AddMany(name, new string[] { town, phone });
+        }
+	}
+
+
+    public void Find(string name)
+    {
+        if (this.phoneBook.Keys.Any(key => key.Contains(name)))
+        {
+            var entry = this.phoneBook.Keys.Where(key => key.Contains(name));
+
+            foreach (var item in entry)
+            {
+                Console.WriteLine("{0} - {1}",item,this.phoneBook[item].ToString()); 
+            }
+            
+        }
+        else
+        {
+            Console.WriteLine("No such item in the phone book!");
+        }
+
+    }
+
+    public void Find(string name,string town)
+    {
+        bool thereIsSuchEntry = false;
+        if (this.phoneBook.Keys.Any(key => key.Contains(name)))
+        {
+            var entry = this.phoneBook.Keys.Where(key => key.Contains(name));            
+
+            foreach (var item in entry)
+            {
+                if (this.phoneBook[item].ToString().Contains(town))
+                {
+                    Console.WriteLine("{0} - {1}", item, this.phoneBook[item].ToString());
+                    thereIsSuchEntry = true;
+                }                                
+            }
+        }
+        else
+        {
+            Console.WriteLine("No such item in the phone book!");
+        }
+
+        if (thereIsSuchEntry == false) // because there can be such name but no such town
+        {
+            Console.WriteLine("No such item in the phone book!");
+        }
+    }    
+}
---
+++ DSA - Dictonaries-Hash-Tables-and-Sets/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using Wintellect.PowerCollections; 
+
+
+class Program
+{
+    static void Main()
+    {
+        PhoneBook phoneBook = new PhoneBook(@"..\..\phones.txt"); //input file
+        phoneBook.Find("Shmatka"); // by town
+        Console.WriteLine("--------------");
+        phoneBook.Find("Shmatka","Plovdiv"); // by name and town
+        Console.WriteLine("--------------");
+        phoneBook.Find("Shmatka", "Chepelare"); // there is no such Town
+    }
+}
