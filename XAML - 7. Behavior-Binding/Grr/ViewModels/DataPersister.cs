713e1b13bf85646afc2f81d4957bc09ef699dbe5
Boris <bonbonev@gmail.com>
2013-09-13 02:12:17 +0300

    XAML 7. Behavior-Binding
    Homework Upload

---
+++ XAML - 7. Behavior-Binding/Grr/ViewModels/DataPersister.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Grr.ViewModels
+{
+    public class DataPersister
+    {
+        public static IEnumerable<AlbumViewModel> GetFromXML(string xmlPath)
+        {
+            XDocument doc = XDocument.Load(xmlPath);
+            var root = doc.Root;
+
+            var gallery =
+                from album in root.Elements("album")
+                select new AlbumViewModel()
+                {
+                    Name = album.Element("name").Value,
+                    Images =
+                        from image in album.Element("images").Elements("image")
+                        select new ImageViewModel() 
+                        {
+                            Title = image.Element("title").Value,
+                            ImageSource = image.Element("imageSource").Value
+                        }
+                };
+
+            return gallery;
+        }
+    }
+}
+
---
+++ XAML - 7. Behavior-Binding/Grr/ViewModels/GalleryViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Grr.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace Grr.ViewModels
+{
+    public class GalleryViewModel
+    {
+        public IEnumerable<AlbumViewModel> Albums { get; set; }
+
+        public GalleryViewModel()
+        {
+            this.Albums = DataPersister.GetFromXML("..\\..\\ImageGallery.xml");            
+        }        
+    }
+}
